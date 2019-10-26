8ac7fbb352d16ad615bbafe99ef1216725c0e913
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-10-25 21:26:32 -0300

    Começando com pwa workbox

---
+++ public/custom-service-worker.js
@@ -0,0 +1,18 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  // /\.js$/,
+  /\.(?:js|css|html)$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  'http://localhost:3000/',
+  new workbox.strategies.NetworkFirst()
+);
